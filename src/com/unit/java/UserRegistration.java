6273c4df609d6e1ad34414587e5549d683fc4ed3
sayrish <sayrrish1@gmail.com>
2022-11-03 10:02:37 +0530

    Added:VAlid Of Passward Rule3

--- src/com/unit/java/UserRegistration.java
+++ src/com/unit/java/UserRegistration.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 import org.junit.Test;
 
 public class userRegistration {
+
 	
 	public boolean firstName(String firstName) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
@@ -13,10 +14,34 @@ public class userRegistration {
 		Matcher matcher = pattern.matcher(firstName);
 		return matcher.matches();
 	}
+
+	
 	public boolean lastName(String lastname) {
 		String regex = "^[A-Z]{1}[a-z]{2,}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(lastname);
+		return matcher.matches();
+	}
+
+	
+	public boolean email(String email) {
+		String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		return matcher.matches();
+	}
+	
+	public boolean mobileNumber(String number) {
+		String regex = "^(91)()[6-9]{1}[0-9]{9}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(number);
+		return matcher.matches();
+	}
+	
+	public boolean password(String password) {
+		String regex = "((?=.*[A-Z]).{8,})";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(password);
 		return matcher.matches();
 	}
 }
